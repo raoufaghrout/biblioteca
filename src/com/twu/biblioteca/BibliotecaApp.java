c21501d2bbe13c065de1bd89d8d5e6d0d2903a08
Raouf Aghrout <aghrout.raouf@gmail.com>
2014-08-22 10:37:26 +0100

    Removed the Magazine as it was unnecessary.

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,19 +1,21 @@
 package com.twu.biblioteca;
 
-import com.twu.biblioteca.library.*;
+import com.twu.biblioteca.library.loan.Book;
+import com.twu.biblioteca.library.Library;
+import com.twu.biblioteca.library.loan.LibraryItem;
+import com.twu.biblioteca.library.loan.Movie;
 import com.twu.biblioteca.utils.BibliotecaOutputPresenter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class BibliotecaApp {
+class BibliotecaApp {
 
     private static List<LibraryItem> libraryCatalogue = new ArrayList<LibraryItem>() {{
         add(new Book("1", "To Kill a Mockingbird", "Harper Lee", "1960"));
         add(new Book("2", "Of Mice and Men", "John Steinbeck", "1937"));
         add(new Movie("3", "Pulp Fiction", "Quentin Tarantino", "1994", "10"));
         add(new Movie("4", "Jurassic Park", "Steven Spielberg", "1993", "unrated"));
-        add(new Magazine("5", "Wired", "Multiple", "2014", "June"));
     }};
 
     public static void main(String[] args) {
